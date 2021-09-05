0258bc2f51143a66764cb9de47fee4f23d7fe36f
Arnob-git <importantfiles22021@gmail.com>
2021-09-05 23:32:00 +0600

    temperature repo

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const API_KEY = `4b1e9ed5668439e217adf037d9f292a4`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+
+    //set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+
+
+}
+
